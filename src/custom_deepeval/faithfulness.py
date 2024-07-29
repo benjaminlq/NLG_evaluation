f3efe9a443e8631433b7ef9e9ef0ebcb7b8f9e17
benjaminlq <benjamin.lequan@gmail.com>
2024-07-29 23:58:53 +0800

    remove truths with full contexts

---
+++ src/custom_deepeval/faithfulness.py
@@ -0,0 +1,151 @@
+from deepeval.metrics.faithfulness.faithfulness import FaithfulnessMetric
+from typing import List, Union
+import asyncio
+
+from deepeval.test_case import (
+    LLMTestCase,
+    LLMTestCaseParams,
+    ConversationalTestCase,
+)
+from deepeval.utils import get_or_create_event_loop, prettify_list
+from deepeval.metrics.utils import (
+    construct_verbose_logs,
+    validate_conversational_test_case,
+    trimAndLoadJson,
+    check_llm_test_case_params,
+)
+
+from deepeval.metrics.faithfulness.template import FaithfulnessTemplate
+from deepeval.metrics.indicator import metric_progress_indicator
+from deepeval.metrics.faithfulness.schema import *
+
+required_params: List[LLMTestCaseParams] = [
+    LLMTestCaseParams.INPUT,
+    LLMTestCaseParams.ACTUAL_OUTPUT,
+    LLMTestCaseParams.RETRIEVAL_CONTEXT,
+]
+
+class CustomFaithfullnessMetric(FaithfulnessMetric):
+    def measure(
+        self, test_case: Union[LLMTestCase, ConversationalTestCase]
+    ) -> float:
+        if isinstance(test_case, ConversationalTestCase):
+            test_case = validate_conversational_test_case(test_case, self)
+        check_llm_test_case_params(test_case, required_params, self)
+
+        self.evaluation_cost = 0 if self.using_native_model else None
+        with metric_progress_indicator(self):
+            if self.async_mode:
+                loop = get_or_create_event_loop()
+                loop.run_until_complete(
+                    self.a_measure(test_case, _show_indicator=False)
+                )
+            else:
+                self.claims = self._generate_claims(test_case.actual_output)
+                self.verdicts = self._generate_verdicts(test_case)
+                self.score = self._calculate_score()
+                self.reason = self._generate_reason()
+                self.success = self.score >= self.threshold
+                self.verbose_logs = construct_verbose_logs(
+                    self,
+                    steps=[
+                        f"Claims:\n{prettify_list(self.claims)}",
+                        f"Verdicts:\n{prettify_list(self.verdicts)}",
+                        f"Score: {self.score}\nReason: {self.reason}",
+                    ],
+                )
+
+                return self.score
+
+    async def a_measure(
+        self,
+        test_case: Union[LLMTestCase, ConversationalTestCase],
+        _show_indicator: bool = True,
+    ) -> float:
+        if isinstance(test_case, ConversationalTestCase):
+            test_case = validate_conversational_test_case(test_case, self)
+        check_llm_test_case_params(test_case, required_params, self)
+
+        self.evaluation_cost = 0 if self.using_native_model else None
+        with metric_progress_indicator(
+            self, async_mode=True, _show_indicator=_show_indicator
+        ):
+            self.claims = await asyncio.gather(
+                self._a_generate_claims(test_case.actual_output)
+            )
+            self.verdicts = await self._a_generate_verdicts(test_case)
+            self.score = self._calculate_score()
+            self.reason = await self._a_generate_reason()
+            self.success = self.score >= self.threshold
+            self.verbose_logs = construct_verbose_logs(
+                self,
+                steps=[
+                    f"Claims:\n{prettify_list(self.claims)}",
+                    f"Verdicts:\n{prettify_list(self.verdicts)}",
+                    f"Score: {self.score}\nReason: {self.reason}",
+                ],
+            )
+
+            return self.score
+        
+    async def _a_generate_verdicts(
+        self, test_case: Union[LLMTestCase, ConversationalTestCase]) -> List[FaithfulnessVerdict]:
+        if len(self.claims) == 0:
+            return []
+
+        verdicts: List[FaithfulnessVerdict] = []
+        prompt = FaithfulnessTemplate.generate_verdicts(
+            claims=self.claims, retrieval_context=test_case.retrieval_context
+        )
+        if self.using_native_model:
+            res, cost = await self.model.a_generate(prompt)
+            self.evaluation_cost += cost
+            data = trimAndLoadJson(res, self)
+            verdicts = [
+                FaithfulnessVerdict(**item) for item in data["verdicts"]
+            ]
+            return verdicts
+        else:
+            try:
+                res: Verdicts = await self.model.a_generate(
+                    prompt, schema=Verdicts
+                )
+                verdicts = [item for item in res.verdicts]
+                return verdicts
+            except TypeError:
+                res = await self.model.a_generate(prompt)
+                data = trimAndLoadJson(res, self)
+                verdicts = [
+                    FaithfulnessVerdict(**item) for item in data["verdicts"]
+                ]
+                return verdicts
+
+    def _generate_verdicts(
+        self, test_case: Union[LLMTestCase, ConversationalTestCase]) -> List[FaithfulnessVerdict]:
+        if len(self.claims) == 0:
+            return []
+
+        verdicts: List[FaithfulnessVerdict] = []
+        prompt = FaithfulnessTemplate.generate_verdicts(
+            claims=self.claims, retrieval_context=test_case.retrieval_context
+        )
+        if self.using_native_model:
+            res, cost = self.model.generate(prompt)
+            self.evaluation_cost += cost
+            data = trimAndLoadJson(res, self)
+            verdicts = [
+                FaithfulnessVerdict(**item) for item in data["verdicts"]
+            ]
+            return verdicts
+        else:
+            try:
+                res: Verdicts = self.model.generate(prompt, schema=Verdicts)
+                verdicts = [item for item in res.verdicts]
+                return verdicts
+            except TypeError:
+                res = self.model.generate(prompt)
+                data = trimAndLoadJson(res, self)
+                verdicts = [
+                    FaithfulnessVerdict(**item) for item in data["verdicts"]
+                ]
+                return verdicts
